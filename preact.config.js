2d099150d1024ba4dc1027572fe9063d78081494
Tomáš Wróbel <wrobel@programmer.net>
2022-05-01 16:50:43 +0200

    Vise 0.0.3-1

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 /**
  * 
  * @param {import("preact-cli").Config} config 
@@ -7,9 +8,9 @@
 export default function(config, env, helpers) {
 	config.resolve.modules.push(env.src);
 
-	for (const loader of helpers.getLoadersByName(config, "css-loader")) {
-		if (typeof loader !== "string" && loader.rule.options?.modules) {
-			loader.loader.options.modules = false;
+	for (const {loader} of helpers.getLoadersByName(config, "css-loader")) {
+		if (typeof loader !== "string" && "modules" in loader.options) {
+			loader.options.modules = false;
 		}
 	}
 }
